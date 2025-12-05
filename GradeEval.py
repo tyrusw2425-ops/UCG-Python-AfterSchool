19a001da50b74a8cf9aa09fda0a8063d2e485f17
tyrusw2425-ops <tyrusw2425@gmail.com>
2025-12-04 17:17:03 -0800

    Fix spelling errors in GradeEval.py
    Corrected spelling errors in program description and input prompt.

--- GradeEval.py
+++ GradeEval.py
@@ -1,7 +1,19 @@
-#Progam Description: This progam eavlutes student grade
+#Program Description: This program eavlutes student grade
 
 #Getting the grade from the user
 
-test_score= float(input("Enter your test score 0-100"))
+test_score = float(input("Enter your test score 0-100: "))
 
-#
+#If and else if statements to assign grade to user depending on score
+#and printing letter grade correctly.
+
+if test_score >= 90:
+    print("Your grade is an A ")
+elif test_score >= 80:
+    print("Your grade is a B")
+elif test_score >= 70:
+    print("Your grade is a C")
+elif test_score >= 60:
+    print("Your grade is a D")
+else:
+    print("Your grade is an F")
